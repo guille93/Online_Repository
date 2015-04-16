8e388c959c486fda7c683e904848e463b3c51290
guille93 <dosa.gcordero@gmail.com>
2015-04-16 20:05:30 +0200

    nuevos laptops

--- Week 2/ordenador/db/seeds.rb
+++ Week 2/ordenador/db/seeds.rb
@@ -1,2 +1,5 @@
 Article.create( title: "HP", Modelo: "Pavilion V",Procesador: "Intel I5", RAM: "8 GB", Precio: "700")
 Article.create( title: "Asus", Modelo: "G7",Procesador: "AMD A6",RAM: "6",Precio: "600")
+Article.create( title: "Toshiba", Modelo: "Satellite",Procesador: "Intel I3", RAM: "4 GB", Precio: "400")
+Article.create( title: "Dell", Modelo: "Premium",Procesador: "AMD A4",RAM: "6",Precio: "550")
+Article.create( title: "Acer", Modelo: "Aspite I",Procesador: "Intel I3", RAM: "4 GB", Precio: "499")
